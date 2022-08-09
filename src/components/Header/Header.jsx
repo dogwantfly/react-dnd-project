4fe886e115439c76b860a5a27bad50d62320813a
hsin-yu <tina2793778@gmail.com>
2022-08-10 00:21:11 +0800

    修改 header menu 功能

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,27 @@
 import { NavLink, Link } from "react-router-dom";
+import { useState } from 'react';
 import './index.css';
 import logo from '../../images/Logo.svg';
 
 
 function Header({ item }) {
   const activeClassName = "text-xl text-[#333333B2] bg-[#F2F2F2] p-4 lg:rounded-2xl font-bold before:translate-x-0 lg:before:content-[''] lg:before:block lg:before:absolute before:bg-[#F2F2F2] lg:before:p-4 lg:before:rounded-3xl lg:before:-z-[1] before:inset-0 relative  lg:before:transition-all active";
-  const openMenu = () => {
-    const menu = document.querySelector('[data-menu]');
-    menu.classList.remove('max-w-0')
-    menu.classList.add('max-w-full')
-  }
-  const closeMenu = () => {
-    const menu = document.querySelector('[data-menu]');
-    menu.classList.remove('max-w-full')
-    menu.classList.add('max-w-0')
-  }
   
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   // https://codepen.io/gcmznt/pen/KBegXZ?editors=0100
   return (
       <header className="container z-10 sticky top-0 flex justify-between mb-[60px] pt-9 items-center">
         <Link to="/" className="lg:hidden">
           <img src={logo} alt="logo" />
         </Link>
-        <button type="button" className="lg:hidden items-center justify-center p-2 rounded-lg text-white focus:outline-none" aria-controls="mobile-menu" aria-expanded="false" onClick={openMenu}>
+        <button type="button" className="lg:hidden items-center justify-center p-2 rounded-lg text-white focus:outline-none" aria-controls="mobile-menu" aria-expanded="false" onClick={() => { setIsMenuOpen(true) }}>
           <span className="sr-only">Open main menu</span>
           <svg className="block h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
-        <div className="lg:w-full lg:flex lg:justify-between lg:items-center fixed right-0 top-0 max-w-0 lg:max-w-none lg:static transition-all ease-in-out bg-[#333333] lg:bg-transparent overflow-hidden" data-menu>
-          <button type="button" className="material-icons text-white absolute left-0 top-0 lg:hidden p-2" onClick={closeMenu}>
+        <div className={`lg:w-full lg:flex lg:justify-between lg:items-center fixed right-0 top-0 lg:max-w-none lg:static transition-all ease-in-out bg-[#333333] lg:bg-transparent overflow-hidden ${isMenuOpen ? 'max-w-full' : 'max-w-0'}`}data-menu>
+          <button type="button" className="material-icons text-white absolute left-0 top-0 lg:hidden p-2" onClick={()=> { setIsMenuOpen(false) }}>
             close
           </button>
           <Link to="/" className="hidden lg:block">
